d67b99c4c0e838a2c7c080c03dc14e7adf915a37
Aymane Benhima <aymanebenhima@gmail.com>
2023-06-04 14:24:54 +0100

    Create IMetier Interface with calcul Method and implementing it

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,25 @@
+package metier;
+
+import dao.IDao;
+
+public class MetierImpl implements IMetier{
+
+    private IDao dao;
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        // Utiliser l'objet dao pour effectuer le calcul
+        double data = dao.getData();
+        double res = data++;
+        System.out.println("Calcul effectué avec les données : " + data);
+        return res;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
